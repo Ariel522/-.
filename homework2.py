b466ad3e9b5c921a76da0a38c9828c0a75064f87
Ariel522 <arik9211@mail.ru>
2024-07-06 23:00:05 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+# Peremennie
+nazvanie_kursa = ('Python,')
+vsego_zadash = (12)
+zatrash_shasov = (1.5)
+vrema_na_1_zadanie = ( zatrash_shasov / vsego_zadash)
+print (nazvanie_kursa, 'всего здач:',vsego_zadash, 'затрачено часов', zatrash_shasov, 'среднее время выполнения', vrema_na_1_zadanie , 'часа.')
